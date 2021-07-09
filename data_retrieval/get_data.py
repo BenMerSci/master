c73167bfed466a9b5e0d8151748c8190126bf97b
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-07-09 12:19:05 -0400

    level up modifications

--- data_retrieval/get_data.py
+++
@@ -1,4 +0,0 @@
-import camelot
-
-tables = camelot.read_pdf("chesapeake.pdf", pages = "13,14", flavor = "stream", strip_text = " .\n", row_tole = 10, table_areas = ["74,680,522,420"], columns = ["100,132,172,210,241,269,301,336,370,407,446,485,529"])
-tables
