cd7d8415c0de3f2139ae5b7e1a62e393020b9e65
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-09-14 10:33:35 -0400

    Changed files path in script 04

--- R/04_get_ecopathdb.R
+++ R/04_get_ecopathdb.R
@@ -8,14 +8,14 @@ model_list <- plyr::ldply(XML::xmlToList(data),data.frame)
 # Load the .csv file that matches my model names to their database model names
 # Merge it with the corresponding models
 # 64 models
-name_match <- read.csv("data/name_match.csv") |>
+name_match <- read.csv("data/raw/name_match.csv") |>
 		base::merge(model_list, by.x = "name_from_db", by.y = "model.model_name", all.x = TRUE) #82 models, has to keep unique ones
 
 # Verified manually the duplicate one to keep the right one
 name_match <- name_match[-c(11,15,17,18,19,22,23,36,37,49,50,55,56,57,60,61,62,69),]
 
 # Load the interactions table
-ecopath_metadata <- readRDS("data/resolved_inter_table.RDS") |>
+ecopath_metadata <- readRDS("data/intermediate/resolved_inter_table.RDS") |>
 		base::merge(name_match, by.x = "model_name", by.y = "model_name", all.x = TRUE) |>
 		dplyr::select(c("model_name","model.geographic_extent","model.model_year",
 		"model.ecosystem_type","model.currency_units")) |>
@@ -33,4 +33,4 @@ ecopath_metadata[which(ecopath_metadata$model_name == "Arctic islands, Zackenber
 ecopath_metadata[which(ecopath_metadata$model_name == "Alto Golfo De California"), "model_year"] <- "1985"
 
 # Write the list as a .Rdata file
-saveRDS(ecopath_metadata, file = "data/ecopath_metadata.RDS")
+saveRDS(ecopath_metadata, file = "data/intermediate/ecopath_metadata.RDS")
