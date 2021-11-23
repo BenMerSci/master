34d82d0a61dcc808e5c682f6ac53146637ba2524
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-11-23 15:36:27 -0500

    Removed interactions only between planktons

--- R/07_create_finaltable.R
+++ R/07_create_finaltable.R
@@ -27,6 +27,10 @@ dataset$ppmr <- dataset$bodymass_pred/dataset$bodymass_prey
 # t/km2 is the same as g/m2
 dataset <- dataset[which(dataset$flux_units %in% c("t/km2/yr","g/m2/yr","g/m2/yr, dry","kg/km2/yr","kg/km2/yr, dry")),]
 
+# Remove interaction that are between plantkons
+# So we focus on interactions wich involves at least
+# one actual species
+dataset <- dataset[-which(grepl("plankt", dataset$prey) & grepl("plankt", dataset$predator)),]
 # Save the dataset
 saveRDS(dataset, "data/clean/dataset.RDS")
 
