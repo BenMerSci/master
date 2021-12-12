7e88d144671e99843666eb2a5e2feccbf1af7c18
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-12-11 21:51:42 -0500

    Commented out species speeds

--- R/05_species_traits.R
+++ R/05_species_traits.R
@@ -17,7 +17,7 @@ organism_class <- lapply(species_list[,"gbif_id"], function(x) taxize::classific
 						   })
 
 
-organism_class <- do.call("rbind.fill", organism_class)
+organism_class <- do.call(plyr::"rbind.fill", organism_class)
 organism_class <- dplyr::select(organism_class, c("X1","X2")) |>
 					dplyr::rename(class = "X1", family = "X2")
 # Bind the classes to the species list
@@ -54,18 +54,18 @@ fish_LW_ls <- lapply(fish_LW_ls, function(x){ data.frame(cbind(unique(x$Species)
 
 fish_LW_ls <- do.call("rbind", fish_LW_ls)
 
-fish_speed <- rfishbase::speed(validated_fish$validated)
-fish_speed_ls <- base::split(fish_speed, f = fish_speed$Species)
-
-fish_speed_ls <- lapply(fish_speed_ls, function(x){ data.frame(cbind(unique(x$Species), mean(x$Speedms, na.rm = TRUE)))})
-fish_speed_ls <- do.call("rbind", fish_speed_ls)
+#fish_speed <- rfishbase::speed(validated_fish$validated)
+#fish_speed_ls <- base::split(fish_speed, f = fish_speed$Species)
+#
+#fish_speed_ls <- lapply(fish_speed_ls, function(x){ data.frame(cbind(unique(x$Species), mean(x$Speedms, na.rm = TRUE)))})
+#fish_speed_ls <- do.call("rbind", fish_speed_ls)
  
 fish_data <- merge(validated_fish, fish_LW_ls, by.x = "validated", by.y = "X1", all.x = TRUE) |>
 	dplyr::rename(bodymass = "X3", bodymass_min = "X2") |>
-	merge(fish_speed_ls, by.x = "validated", by.y = "X1", all.x = TRUE) |>
-	dplyr::rename(speed = "X2") |>
+	#merge(fish_speed_ls, by.x = "validated", by.y = "X1", all.x = TRUE) |>
+	#dplyr::rename(speed = "X2") |>
 	merge(fish, by.x = "original", by.y = "scientific_name", all.x = TRUE) |>
-	subset(select = c("original", "gbif_id", "class", "family", "bodymass", "bodymass_min", "speed")) |>
+	subset(select = c("original", "gbif_id", "class", "family", "bodymass", "bodymass_min")) |>
 	dplyr::rename(scientific_name = "original")
 
 ##### Sealife #####
@@ -138,7 +138,7 @@ plankton_data[which(plankton_data$scientific_name == "Bakterioplankton"), "bodym
 species_traits <- plyr::rbind.fill(fish_data, sealife_data, aves_data, mammalia_data, insecta_data, plankton_data)
 species_traits[which(species_traits$bodymass == NaN), "bodymass"] <- NA
 species_traits[which(species_traits$bodymass_min == NaN), "bodymass_min"] <- NA
-species_traits[which(species_traits$speed == NaN), "speed"] <- NA
+#species_traits[which(species_traits$speed == NaN), "speed"] <- NA
 
 # Getting missing length of remaining fish from the table `Species` from Fishbase
 # With this lengthMax, will apply the length_weight relationship from the length_weight table
@@ -156,67 +156,67 @@ miss_fish_length_LS <- do.call("rbind", miss_fish_length_LS) |>
 			dplyr::rename(scientific_name = "X1")
 miss_fish_length_LS$bodymass <- as.numeric(miss_fish_length_LS$a)*(as.numeric(miss_fish_length_LS$Length)^as.numeric(miss_fish_length_LS$b))
 
- species_traits[which(species_traits$scientific_name %in% miss_fish_length_LS$scientific_name), "bodymass"] <- miss_fish_length_LS$bodymass
+species_traits[which(species_traits$scientific_name %in% miss_fish_length_LS$scientific_name), "bodymass"] <- miss_fish_length_LS$bodymass
 
 # Locomotion from https://doi.org/10.1002/ecy.3114
-loco <- read.csv("data/raw/locomotion_ecology/Animal_Locomotion_Database_ecy0011-sup-0001-DataS1.txt", header  = TRUE, sep = "\t") |>
-	dplyr::select(c("Taxa_ID","SI_Mass","SI_Length","Trait_ID","Trait_Value","Temperature","Temp_Type","Thermy")) |>
-	dplyr::filter(Taxa_ID %in% species_traits$scientific_name) |>
-	(\(x)base::split(x, f = x$Taxa_ID))()
-
-temp_loco <- loco[c(8,12,28,32)]
-
-loco <- lapply(loco, function(x) tidyr::pivot_wider(x, names_from = "Trait_ID", values_from = "Trait_Value"))
-
-for(i in 1:length(temp_loco)){
-	temp <- temp_loco[[i]]
-	temp <- base::split(temp, temp$Trait_ID)
-	temp <- lapply(temp, function(x) data.frame(cbind(unique(x$Taxa_ID), mean(x$SI_Mass, na.rm = TRUE), mean(x$SI_Length, na.rm = TRUE), unique(x$Trait_ID), mean(x$Trait_Value, na.rm = TRUE), mean(x$Temperature, na.rm = TRUE), unique(x$Temp_Type), unique(x$Thermy))))
-	temp <- lapply(temp, function(x) dplyr::rename(x, Taxa_ID = "X1", SI_Mass = "X2", SI_Length = "X3", Trait_ID = "X4", Trait_Value = "X5", Temperature = "X6", Temp_Type = "X7", Thermy = "X8"))
-	temp <- do.call("rbind", temp)
-	temp <- tidyr::pivot_wider(temp, names_from = "Trait_ID", values_from = "Trait_Value")
-	temp_loco[[i]] <- temp
-
-}
-
-temp_loco[[1]] <- data.frame(cbind(unique(temp_loco[[1]]$Taxa_ID), mean(as.numeric(temp_loco[[1]]$SI_Mass)), mean(as.numeric(temp_loco[[1]]$SI_Length)), mean(as.numeric(temp_loco[[1]]$Temperature)), unique(temp_loco[[1]]$Temp_Type), unique(temp_loco[[1]]$Thermy), temp_loco[[1]][1,"Attack Velocity"], temp_loco[[1]][2,"Escape Velocity"]))
-temp_loco[[1]] <- dplyr::rename(temp_loco[[1]], Taxa_ID = "unique.temp_loco..1...Taxa_ID.", SI_Mass = "mean.as.numeric.temp_loco..1...SI_Mass..", SI_Length = "mean.as.numeric.temp_loco..1...SI_Length..", Temperature = "mean.as.numeric.temp_loco..1...Temperature..", Temp_Type = "unique.temp_loco..1...Temp_Type.", Thermy = "unique.temp_loco..1...Thermy.", `Attack Velocity` = "Attack.Velocity", `Escape Velocity`= "Escape.Velocity")
-temp_loco[[3]] <- data.frame(cbind(unique(temp_loco[[3]]$Taxa_ID), mean(as.numeric(temp_loco[[3]]$SI_Mass)), mean(as.numeric(temp_loco[[3]]$SI_Length)), mean(as.numeric(temp_loco[[3]]$Temperature)), unique(temp_loco[[3]]$Temp_Type), unique(temp_loco[[3]]$Thermy), temp_loco[[3]][1,"Acceleration"], temp_loco[[3]][1,"Escape Velocity"], temp_loco[[3]][2,"Turn Radius"]))
-temp_loco[[3]] <- dplyr::rename(temp_loco[[3]], Taxa_ID = "unique.temp_loco..3...Taxa_ID.", SI_Mass = "mean.as.numeric.temp_loco..3...SI_Mass..", SI_Length = "mean.as.numeric.temp_loco..3...SI_Length..", Temperature = "mean.as.numeric.temp_loco..3...Temperature..", Temp_Type = "unique.temp_loco..3...Temp_Type.", Thermy = "unique.temp_loco..3...Thermy.", Acceleration = "Acceleration", `Escape Velocity` = "Escape.Velocity", `Turn Radius` = "Turn.Radius")
-
-loco[c(8,12,28,32)] <- temp_loco[c(1,2,3,4)]
-
-loco_categorical <- lapply(loco, function(x) data.frame(cbind(unique(x$Taxa_ID), unique(x$Temp_Type), unique(x$Thermy))))
-
-loco_categorical[[10]] <- loco_categorical[[10]][1,]
-loco_categorical[[31]] <- loco_categorical[[31]][2,]
-loco_categorical <- plyr::rbind.fill(loco_categorical)
-loco_categorical <- dplyr::rename(loco_categorical, c(Taxa_ID = "X1", Temp_Type = "X2", Thermy = "X3"))
-loco_num <- lapply(loco, function(x) dplyr::select(x, -c("Taxa_ID","Temp_Type","Thermy")))
-
-loco_num <- lapply(loco_num, function(x) {
-				x <- as.data.frame(x)
-				get_col <- colnames(x)
-				x[get_col] <- sapply(x[get_col], as.numeric)
-				x <- t(data.frame(colMeans(x, na.rm = TRUE)))
-				x <- as.data.frame(x)
-				return(x)
-})
-
-loco_num <- plyr::rbind.fill(loco_num)
-loco <- cbind(loco_categorical, loco_num)
-loco <- dplyr::rename(loco, c(temp_type = "Temp_Type", thermy = "Thermy", si_mass = "SI_Mass", si_length = "SI_Length", temp_loco = "Temperature", acceleration = "Acceleration", attack_velocity = "Attack Velocity", voluntary_body_velocity = "Voluntary Body Velocity", escape_velocity = "Escape Velocity", turn_radius = "Turn Radius", angular_velocity = "Angular Velocity"))
-
-#species_traits <- merge(species_traits, loco, by.x = "scientific_name", by.y = "Taxa_ID", all.x = TRUE)
-
-# Merge species_traits with locomotion data
-loco <- loco[which(loco$Taxa_ID %in% species_traits[which(species_traits$scientific_name %in% loco$Taxa_ID & is.na(species_traits$speed)), ]$scientific_name ),] |>
-	dplyr::select(c("Taxa_ID", "voluntary_body_velocity")) |>
-	dplyr::rename(scientific_name = "Taxa_ID", speed = "voluntary_body_velocity")
-species_traits <- merge(species_traits, loco, by="scientific_name", all.x = TRUE)
-species_traits[which(is.na(species_traits$speed.x) & !is.na(species_traits$speed.y)),"speed.x"] <- species_traits[which(is.na(species_traits$speed.x) & !is.na(species_traits$speed.y)),"speed.y"] 
-species_traits <- species_traits[,c("scientific_name","gbif_id","class","family","bodymass","bodymass_min","speed.x")] |>
-			dplyr::rename(speed = "speed.x")
+#loco <- read.csv("data/raw/locomotion_ecology/Animal_Locomotion_Database_ecy0011-sup-0001-DataS1.txt", header  = TRUE, sep = "\t") |>
+#	dplyr::select(c("Taxa_ID","SI_Mass","SI_Length","Trait_ID","Trait_Value","Temperature","Temp_Type","Thermy")) |>
+#	dplyr::filter(Taxa_ID %in% species_traits$scientific_name) |>
+#	(\(x)base::split(x, f = x$Taxa_ID))()
+#
+#temp_loco <- loco[c(8,12,28,32)]
+#
+#loco <- lapply(loco, function(x) tidyr::pivot_wider(x, names_from = "Trait_ID", values_from = "Trait_Value"))
+#
+#for(i in 1:length(temp_loco)){
+#	temp <- temp_loco[[i]]
+#	temp <- base::split(temp, temp$Trait_ID)
+#	temp <- lapply(temp, function(x) data.frame(cbind(unique(x$Taxa_ID), mean(x$SI_Mass, na.rm = TRUE), mean(x$SI_Length, na.rm = TRUE), unique(x$Trait_ID), mean(x$Trait_Value, na.rm = TRUE), mean(x$Temperature, na.rm = TRUE), unique(x$Temp_Type), unique(x$Thermy))))
+#	temp <- lapply(temp, function(x) dplyr::rename(x, Taxa_ID = "X1", SI_Mass = "X2", SI_Length = "X3", Trait_ID = "X4", Trait_Value = "X5", Temperature = "X6", Temp_Type = "X7", Thermy = "X8"))
+#	temp <- do.call("rbind", temp)
+#	temp <- tidyr::pivot_wider(temp, names_from = "Trait_ID", values_from = "Trait_Value")
+#	temp_loco[[i]] <- temp
+#
+#}
+#
+#temp_loco[[1]] <- data.frame(cbind(unique(temp_loco[[1]]$Taxa_ID), mean(as.numeric(temp_loco[[1]]$SI_Mass)), mean(as.numeric(temp_loco[[1]]$SI_Length)), mean(as.numeric(temp_loco[[1]]$Temperature)), unique(temp_loco[[1]]$Temp_Type), unique(temp_loco[[1]]$Thermy), temp_loco[[1]][1,"Attack Velocity"], temp_loco[[1]][2,"Escape Velocity"]))
+#temp_loco[[1]] <- dplyr::rename(temp_loco[[1]], Taxa_ID = "unique.temp_loco..1...Taxa_ID.", SI_Mass = "mean.as.numeric.temp_loco..1...SI_Mass..", SI_Length = "mean.as.numeric.temp_loco..1...SI_Length..", Temperature = "mean.as.numeric.temp_loco..1...Temperature..", Temp_Type = "unique.temp_loco..1...Temp_Type.", Thermy = "unique.temp_loco..1...Thermy.", `Attack Velocity` = "Attack.Velocity", `Escape Velocity`= "Escape.Velocity")
+#temp_loco[[3]] <- data.frame(cbind(unique(temp_loco[[3]]$Taxa_ID), mean(as.numeric(temp_loco[[3]]$SI_Mass)), mean(as.numeric(temp_loco[[3]]$SI_Length)), mean(as.numeric(temp_loco[[3]]$Temperature)), unique(temp_loco[[3]]$Temp_Type), unique(temp_loco[[3]]$Thermy), temp_loco[[3]][1,"Acceleration"], temp_loco[[3]][1,"Escape Velocity"], temp_loco[[3]][2,"Turn Radius"]))
+#temp_loco[[3]] <- dplyr::rename(temp_loco[[3]], Taxa_ID = "unique.temp_loco..3...Taxa_ID.", SI_Mass = "mean.as.numeric.temp_loco..3...SI_Mass..", SI_Length = "mean.as.numeric.temp_loco..3...SI_Length..", Temperature = "mean.as.numeric.temp_loco..3...Temperature..", Temp_Type = "unique.temp_loco..3...Temp_Type.", Thermy = "unique.temp_loco..3...Thermy.", Acceleration = "Acceleration", `Escape Velocity` = "Escape.Velocity", `Turn Radius` = "Turn.Radius")
+#
+#loco[c(8,12,28,32)] <- temp_loco[c(1,2,3,4)]
+#
+#loco_categorical <- lapply(loco, function(x) data.frame(cbind(unique(x$Taxa_ID), unique(x$Temp_Type), unique(x$Thermy))))
+#
+#loco_categorical[[10]] <- loco_categorical[[10]][1,]
+#loco_categorical[[31]] <- loco_categorical[[31]][2,]
+#loco_categorical <- plyr::rbind.fill(loco_categorical)
+#loco_categorical <- dplyr::rename(loco_categorical, c(Taxa_ID = "X1", Temp_Type = "X2", Thermy = "X3"))
+#loco_num <- lapply(loco, function(x) dplyr::select(x, -c("Taxa_ID","Temp_Type","Thermy")))
+#
+#loco_num <- lapply(loco_num, function(x) {
+#				x <- as.data.frame(x)
+#				get_col <- colnames(x)
+#				x[get_col] <- sapply(x[get_col], as.numeric)
+#				x <- t(data.frame(colMeans(x, na.rm = TRUE)))
+#				x <- as.data.frame(x)
+#				return(x)
+#})
+#
+#loco_num <- plyr::rbind.fill(loco_num)
+#loco <- cbind(loco_categorical, loco_num)
+#loco <- dplyr::rename(loco, c(temp_type = "Temp_Type", thermy = "Thermy", si_mass = "SI_Mass", si_length = "SI_Length", temp_loco = "Temperature", acceleration = "Acceleration", attack_velocity = "Attack Velocity", voluntary_body_velocity = "Voluntary Body Velocity", escape_velocity = "Escape Velocity", turn_radius = "Turn Radius", angular_velocity = "Angular Velocity"))
+#
+##species_traits <- merge(species_traits, loco, by.x = "scientific_name", by.y = "Taxa_ID", all.x = TRUE)
+#
+## Merge species_traits with locomotion data
+#loco <- loco[which(loco$Taxa_ID %in% species_traits[which(species_traits$scientific_name %in% loco$Taxa_ID & is.na(species_traits$speed)), ]$scientific_name ),] |>
+#	dplyr::select(c("Taxa_ID", "voluntary_body_velocity")) |>
+#	dplyr::rename(scientific_name = "Taxa_ID", speed = "voluntary_body_velocity")
+#species_traits <- merge(species_traits, loco, by="scientific_name", all.x = TRUE)
+#species_traits[which(is.na(species_traits$speed.x) & !is.na(species_traits$speed.y)),"speed.x"] <- species_traits[which(is.na(species_traits$speed.x) & !is.na(species_traits$speed.y)),"speed.y"] 
+#species_traits <- species_traits[,c("scientific_name","gbif_id","class","family","bodymass","bodymass_min","speed.x")] |>
+#			dplyr::rename(speed = "speed.x")
 
 # Other species with missing bodymass
 #miss_sp_bm <- species_traits[which(is.na(species_traits$bodymass)),]
