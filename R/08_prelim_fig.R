1e5e9608d7c86df689843f3eb7f64332e195e943
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2021-12-12 14:45:51 -0500

    Changed x-axis for Ni*Nj*Mi

--- R/08_prelim_fig.R
+++ R/08_prelim_fig.R
@@ -4,13 +4,8 @@ library(ggplot2)
 # Load the data
 dataset <- readRDS("data/clean/dataset.RDS")
 
-# Remove NAs
-dataset <- dplyr::select(dataset, -c("speed_prey","speed_pred"))
-dataset <- na.omit(dataset)
-dataset <- dataset[which(dataset$energy_flow < 50),]
 # Check interactions where biomass prey is lower than biomass flux
-bigger_flux <- dataset[which(dataset$biomass_prey < dataset$energy_flow),]
-
+#bigger_flux <- dataset[which(dataset$pred_flow > dataset$biomass_prey),]
 
 terrestrial <- dataset[which(dataset$habitat_type == "terrestrial"),]
 freshwater <- dataset[which(dataset$habitat_type == "freshwater"),]
@@ -18,49 +13,54 @@ marine <- dataset[which(dataset$habitat_type == "marine"),]
 
 ##########################################################################
 # Plots different habitat type
-ggplot(data = dataset, aes(x = ppmr, y = energy_flow, color = habitat_type)) + 
-scale_color_manual(values= c("marine" = "deepskyblue3", "freshwater" = "sandybrown", "terrestrial" = "olivedrab")) +
-geom_point()
-# Adjust plot frames
+
+# Create axis label
+x_exp <- expression(N[i] * N[j] ~ M[i])
+x_exp_log <- expression(log(N[i] * N[j] ~ M[i]))
+
 # terrestrial
-plot1 <- ggplot(data = terrestrial, aes(x = ppmr, y = energy_flow)) + 
+plot1 <- ggplot(data = terrestrial, aes(x = ((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred)*bodymass_prey), y = pred_flow)) + 
 geom_point(color = "olivedrab") +
 theme(legend.position = "none") +
-scale_y_continuous("Flux de biomasse (g/m²/année)") +
+labs(y = "Flux biomasse (g/m²/année)", x = x_exp) +
 ggtitle("Terrestre") +
 theme_bw()
 # freshwater
-plot2 <- ggplot(data = freshwater, aes(x = ppmr, y = energy_flow)) + 
+plot2 <- ggplot(data = freshwater, aes(x = ((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred)*bodymass_prey), y = pred_flow)) + 
 geom_point(color = "sandybrown") +
 theme(legend.position = "none", axis.title.y=element_blank()) +
+labs(y = "", x = x_exp) +
 ggtitle("Aquatique") +
 theme_bw()
 # marine
-plot3 <- ggplot(data = dataset, aes(x = ppmr, y = energy_flow)) + 
+plot3 <- ggplot(data = dataset, aes(x = ((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred)*bodymass_prey), y = pred_flow)) + 
 geom_point(color = "deepskyblue3") +
 theme(legend.position = "none", axis.title.y=element_blank()) +
+labs(y = "", x = x_exp) +
 ggtitle("Marin") +
 theme_bw()
 # log-terrestrial
-plot4 <- ggplot(data = terrestrial, aes(x = log(ppmr), y = log(energy_flow))) + 
+plot4 <- ggplot(data = terrestrial, aes(x = log(((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred))*bodymass_prey), y = log(pred_flow))) + 
 geom_point(color = "olivedrab") +
 theme(legend.position = "none") +
-scale_y_continuous("log(Flux de biomasse (g/m²/année))") +
+labs(y = "log(Flux biomasse (g/m²/année))", x = x_exp_log) +
 theme_bw()
 # log-freshwater
-plot5 <- ggplot(data = freshwater, aes(x = log(ppmr), y = log(energy_flow))) + 
+plot5 <- ggplot(data = freshwater, aes(x = log(((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred))*bodymass_prey), y = log(pred_flow))) + 
 geom_point(color = "sandybrown") +
 theme(legend.position = "none", axis.title.y=element_blank()) +
+labs(y = "", x = x_exp_log) +
 theme_bw()
 # log-marine
-plot6 <- ggplot(data = dataset, aes(x = log(ppmr), y = log(energy_flow))) + 
+plot6 <- ggplot(data = dataset, aes(x = log(((biomass_prey/bodymass_prey)*(biomass_pred/bodymass_pred))*bodymass_prey), y = log(pred_flow))) + 
 geom_point(color = "deepskyblue3") +
 theme(legend.position = "none", axis.title.y=element_blank()) +
+labs(y = "", x = x_exp_log) +
 theme_bw()
 
 # Arrange plots
 plots <- gridExtra::grid.arrange(plot1, plot2, plot3, plot4, plot5, plot6, nrow = 2, ncol=3)
-
+ 
 # Save
 ggsave("../master_talk/images/flux_ppmr.png",plot = plots, width = 8, height = 8, dpi = "retina")
-ggsave("../manuscripts/ms_trophic_fluxes/figures/flux_ppmr.png", width = 9, height = 5, dpi = "retina")
+ggsave("../manuscripts/ms_trophic_fluxes/figures/flux_ppmr.png",plot = plots, width = 9, height = 5, dpi = "retina")
