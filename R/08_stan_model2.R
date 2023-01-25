722a832e1991314d5ae9f738e144e791ef476d82
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2023-01-24 20:53:01 -0500

    Added plots to model2 R script

--- R/08_stan_model2.R
+++ R/08_stan_model2.R
@@ -33,6 +33,7 @@ library(ggdist)
 library(ggridges)
 library(gridExtra)
 library(patchwork)
+library(hrbrthemes)
 
 # Load the model
 dataset <- readRDS("data/clean/new_dataset.RDS")
@@ -43,6 +44,66 @@ output_stan_model2 <- readRDS("results/model_outputs/output_stan_model2.RDS")
 output_stan_model2 <- recover_types(output_stan_model2)
 # Draw the posterior for all parameters
 general_params <- output_stan_model2 |>
-                   tidybayes::gather_draws(a_pop, a_sd, sigma)
+                   tidybayes::gather_draws(mu_alpha, sd_alpha, sigma)
 
-a_grp <- output_stan_model2 |> tidybayes::gather_draws(a_grp[pred_id])
+alpha <- output_stan_model2 |> tidybayes::gather_draws(alpha[pred_id])
+
+# Get pred_id & habitat_type to join with the stan output
+pred_ids <- unique(dataset[, c("pred_id", "habitat_type", "trophic_guild")])
+
+alpha <-  dplyr::left_join(alpha, pred_ids, by = "pred_id") |>
+          dplyr::mutate(pred_id = as.factor(pred_id),
+                        habitat_type = as.factor(habitat_type),
+                        trophic_guild = as.factor(trophic_guild))
+
+pred_ids <- pred_ids |> dplyr::mutate(pred_id = as.factor(pred_id),
+                                      habitat_type = as.factor(habitat_type),
+                                      trophic_guild = as.factor(trophic_guild))
+
+pred_ids_guild <- pred_ids |> dplyr::arrange(trophic_guild)
+
+desired_order <- c("Small mammal herbivore","Large mammal herbivore","Small mammal predator",
+                   "Medium mammal predator","Large mammal predator","Predatory birds",
+                   "Non-predatory birds","Small reef-coast","Medium reef-coast",
+                   "Small pelagic omnivore","Medium pelagic omnivore",
+                   "Small pelagic carnivore","Medium pelagic carnivore",
+                   "Small demersal omnivore","Medium demersal omnivore",
+                   "Small demersal carnivore","Medium demersal carnivore",
+                   "Reptiles","Sharks","Cephalopods","Shrimps",
+                   "Mollusc and crustacea","Invertebrates","Plankton")
+
+alpha$trophic_guild <- factor(as.character(alpha$trophic_guild), levels=desired_order)
+
+alpha <- alpha[order(alpha$trophic_guild),]
+alpha <- alpha |> dplyr::arrange(trophic_guild)
+
+ggplot(alpha, aes(x = `.value`, y = pred_id, fill = trophic_guild)) +
+  geom_density_ridges_gradient(scale = 5, rel_min_height = 0.01) +
+     theme_ipsum() +
+      theme(
+       legend.title = element_text(size = 15),
+       legend.text = element_text(size = 14),
+       axis.title.x = element_text(size = 20),
+       axis.text.x = element_text(size = 18),
+       axis.text.y = element_blank()
+      ) +
+      xlim(c(-20, 10)) +
+       ylab("") +
+       xlab("Space clearance rate (km²/individual*year)") +
+       labs(fill = "Trophic guild") +
+        scale_y_discrete(guide = guide_axis(n.dodge = 2), limits = alpha$pred_id)# +
+        #scale_color_manual(values=c("Small mammal herbivore"="palegreen",
+        #                            "Large mammal herbivore"="palegreen3",
+        #                            "Small mammal predator"="lightpink",
+        #                            "Medium mammal predator"="lightcoral",
+        #                            "Large mammal predator"="indianred4",
+        #                            "Small reef-coast"="snow3",
+        #                            "Medium reef-coast"="snow4",
+        #                            "Non-predatory birds"="deepskyblue3",
+        #                            "Predatory birds"="deepskyblue4",
+        #                            "Small demersal carnivore"="",
+        #                            "Medium demersal carnivore"="",
+        #                            ""
+        #                            ))
+
+plot_sim_error(output_stan_model2)
--- R/10_plot_func.R
+++ R/10_plot_func.R
@@ -1,7 +1,7 @@
 # One-one plot for simulation and observations
 one_one_plot <- function(dataset, num_model) {
       dataset |>
-        ggplot(aes(x = log(pred_flow), y = .data[[paste0("yrep_", num_model, "_mean")]])) +
+        ggplot(aes(x = log(biomass_flow), y = .data[[paste0("yrep_", num_model, "_mean")]])) +
         geom_point() +
         theme_minimal() +
         theme(
@@ -21,64 +21,68 @@ one_one_plot <- function(dataset, num_model) {
 # Plots with dist. posterior intervals against observations with colors
 plot_sim_noerror <- function(model) {
   df <- model |>
-        tidybayes::gather_rvars(log_pred_flow_hat[id]) |>
+        tidybayes::gather_rvars(log_biomass_flow_hat[id]) |>
         dplyr::left_join(dataset |> mutate(id = row_number())) |>
         dplyr::mutate(habitat_type = as.factor(habitat_type))
-  
-  levels(df$habitat_type) <- list("Terrestre" = "terrestrial",
-                                         "Aquatique" = "freshwater",
-                                         "Marin" = "marine",
-                                         "Marin & aquatique" = "marine_freshwater")
 
-  df |> ggplot(aes(x= log(biomass_prey) + log(abundance_predator), dist=.value), col="Posterior distribution") +
+  levels(df$habitat_type) <- list("Terrestrial" = "terrestrial",
+                                         "Freshwater" = "freshwater",
+                                         "Marine" = "marine",
+                                         "Marine & freshwater" = "marine_freshwater")
+
+  df |> ggplot(aes(x= log(biomass_prey) + log(abundance_predator), dist=.value, col="Model predictions")) +
         stat_dist_pointinterval() +
         geom_point(aes(x=log(biomass_prey) + log(abundance_predator),
-         y=log(pred_flow), col=habitat_type), inherit.aes=FALSE, size=3, alpha=0.8) +
+         y=log(biomass_flow), col=habitat_type), inherit.aes=FALSE, size=3, alpha=0.8) +
         theme_minimal() +
         theme(
          legend.position = "right",
          legend.title = element_blank(),
-         plot.title = element_text(size = 15),
-         axis.title.y = element_text(size = 15),
-         axis.title.x = element_text(size = 15),
-         axis.text.x = element_text(size = 13),
-         axis.text.y = element_text(size = 13)
+         legend.text = element_text(size = 30),
+         plot.title = element_blank(),
+         axis.title.y = element_text(size = 30),
+         axis.title.x = element_text(size = 30),
+         axis.text.x = element_text(size = 25),
+         axis.text.y = element_text(size = 25)
         ) +
-        labs(title = "Flux de biomasse observés vs prédits (échelle log)") +
+        labs(title = "Biomass flow observed vs prediction (log-scale)") +
         xlab(expression(log(B[i] * N[j]))) +
-        ylab("Flux de biomasse (tonnes/km²*année)") +
-        scale_colour_manual(name="Line color", values=c(`Posterior distribution`="black", "Terrestre"="olivedrab",
-                            "Aquatique" = "sandybrown", "Marin"="deepskyblue3", "Marin & aquatique"="purple"))
+        ylab("log(Biomass flow (tons/km²*year))") +
+        scale_colour_manual(values=c("Model predictions"="black", "Terrestrial"="olivedrab",
+                            "Freshwater" = "sandybrown", "Marine"="deepskyblue3", "Marine & freshwater"="purple"),
+                            limits= c("Model predictions","Terrestrial","Freshwater","Marine","Marine & freshwater"))
 }
 
 plot_sim_error <- function(model) {
+      
   df <- model |>
         tidybayes::gather_rvars(y_rep[id]) |>
         dplyr::left_join(dataset |> mutate(id = row_number())) |>
         dplyr::mutate(habitat_type = as.factor(habitat_type))
 
-  levels(df$habitat_type) <- list("Terrestre" = "terrestrial",
-                                         "Aquatique" = "freshwater",
-                                         "Marin" = "marine",
-                                         "Marin & aquatique" = "marine_freshwater")
+  levels(df$habitat_type) <- list("Terrestrial" = "terrestrial",
+                                         "Freshwater" = "freshwater",
+                                         "Marine" = "marine",
+                                         "Marine & freshwater" = "marine_freshwater")
 
-  df |> ggplot(aes(x= log(biomass_prey) + log(abundance_predator), dist=.value), col="Posterior distribution") +
+  df |> ggplot(aes(x= log(biomass_prey) + log(abundance_predator), dist=.value, col="Model predictions")) +
         stat_dist_pointinterval() +
         geom_point(aes(x=log(biomass_prey) + log(abundance_predator),
-         y=log(pred_flow), col=habitat_type), inherit.aes=FALSE, size=3, alpha=0.8) +
+         y=log(biomass_flow), col=habitat_type), inherit.aes=FALSE, size=3, alpha=0.8) +
         theme_minimal() +
         theme(
-         legend.position = "right",
          legend.title = element_blank(),
-         plot.title = element_text(size = 15),
-         axis.title.y = element_text(size = 15),
-         axis.title.x = element_text(size = 15),
-         axis.text.x = element_text(size = 13),
-         axis.text.y = element_text(size = 13)
+         legend.text = element_text(size = 15),
+         legend.position = "bottom",
+         axis.title.y = element_text(size = 17),
+         axis.title.x = element_text(size = 17),
+         axis.text.x = element_text(size = 15),
+         axis.text.y = element_text(size = 15)
         ) +
-        labs(title = "Flux de biomasse observés vs prédits (échelle log)") +
+        #labs(title = "Modèle 0 - Flux de biomasse observés vs prédits (échelle log)") +
         xlab(expression(log(B[i] * N[j]))) +
-        ylab("Flux de biomasse (tonnes/km²*année)") +
-        scale_colour_manual(name="Line color", values=c(`Posterior distribution`="black", "Terrestre"="olivedrab",
-                            "Aquatique" = "sandybrown", "Marin"="deepskyblue3", "Marin & aquatique"="purple"))
+        ylab(expression(log(F[ij]) (ton/km^2*year))) +
+        scale_colour_manual(values=c("Model predictions"="black", "Terrestrial"="olivedrab",
+                            "Freshwater" = "sandybrown", "Marine"="deepskyblue3", "Marine & freshwater"="purple"),
+                            limits=(c("Model predictions","Terrestrial","Freshwater","Marine","Marine & freshwater")))
 }
