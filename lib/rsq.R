8e7632964ea8d84fb99736fa03c9a96bdc0f6952
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2023-05-01 12:17:08 -0400

    Removed xlim limit for Rsq plot

--- lib/rsq.R
+++ lib/rsq.R
@@ -23,8 +23,8 @@ plot_rsq <- function(list_model) {
 
         df <- do.call(cbind, df_list)
 
-        rsq_plot <- bayesplot::mcmc_areas(df, area_method = "equal height") +
-                    ggplot2::xlim(0, 1)
+        rsq_plot <- bayesplot::mcmc_areas(df, area_method = "equal height")# +
+                    #ggplot2::xlim(0, 1)
 
   return(rsq_plot)
 }
